ca434a4f5aed73a646216963573b3fe56d0fc620
LindsayKXW <klingxi@bu.edu>
2021-12-09 00:16:21 -0500

    update sim.py

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -11,8 +11,21 @@ def manhattan_dist(x, y):
     return res
 
 def jaccard_dist(x, y):
+    x_set = set(x)
+    y_set = set(y)
+    jac_dist = len(x_set.intersection(y_set)) / len(x_set.union(y_set))
+    return 1-jac_dist
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dot_product = 0
+    nx = 0
+    ny = 0
+
+    for a, b in zip(x,y):
+        dot_product += a*b
+        nx += a**2
+        ny += b**2
+
+    return dot_product/((nx * ny) **0.5)
 
 # Feel free to add more
